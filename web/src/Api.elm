0b0eb2124203170b95b225d01ffd117dc5c26514
Andrey Hetman <magackame@gmail.com>
2023-04-13 21:33:42 +0300

    Update API prefix

--- web/src/Api.elm
+++ web/src/Api.elm
@@ -1,11 +1,11 @@
-module Api exposing (prefix, pfpUrl, encodeMaybe)
-
+module Api exposing (encodeMaybe, pfpUrl, prefix)
 
 import Json.Encode as E
 
 
 prefix : String
-prefix = "http://localhost:3000/api"
+prefix =
+    "/api"
 
 
 pfpUrl : Maybe String -> String
@@ -13,10 +13,11 @@ pfpUrl profilePictureFileName =
     case profilePictureFileName of
         Just fileName ->
             prefix ++ "/images/" ++ fileName
+
         Nothing ->
             "https://www.wiresmithtech.com/wp-content/uploads/rust-logo-512x512-1.png"
 
-            
+
 encodeMaybe : (a -> E.Value) -> Maybe a -> E.Value
 encodeMaybe e m =
     case m of
