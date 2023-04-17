7e2f2b7cd2f4c0e67c640b7c68e310e283924189
Andrey Hetman <magackame@gmail.com>
2023-04-17 16:10:42 +0300

    Add status check, so that only approved translations show up in translations list

--- server/src/db/translation.rs
+++ server/src/db/translation.rs
@@ -1,4 +1,5 @@
 use super::id::Id;
+use super::post::status::Status as PostStatus;
 use serde::Serialize;
 use sqlx::mysql::MySqlPool;
 use sqlx::Result;
@@ -15,16 +16,19 @@ impl Translation {
         sqlx::query_as!(
             Translation,
             "
-        SELECT
-            post_contents.id AS post_content_id,
-            languages.id AS language_id,
-            name AS language
-        FROM posts
-            JOIN post_contents ON posts.id = post_contents.post_id
-            JOIN languages ON post_contents.language_id = languages.id
-        WHERE posts.id = ?
-        ",
-            post_id
+            SELECT
+                post_contents.id AS post_content_id,
+                languages.id AS language_id,
+                name AS language
+            FROM posts
+                JOIN post_contents ON posts.id = post_contents.post_id
+                JOIN languages ON post_contents.language_id = languages.id
+            WHERE
+                posts.id = ?
+                AND status = ?
+            ",
+            post_id,
+            PostStatus::Approved.as_str(),
         )
         .fetch_all(db_pool)
         .await
